829fecf045ef50b985d60e360ba0d49cd5b9accc
colussicode <colussidev@gmail.com>
2023-08-09 20:42:07 -0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomBar : BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        val paceFragment = PaceFragment.newInstance()
+        val historyFragment = HistoryFragment.newInstance()
+
+        bottomBar.setOnItemSelectedListener { menuItem ->
+            val fragment = when(menuItem.itemId) {
+                R.id.pace_fragment -> paceFragment
+                R.id.history_fragment -> historyFragment
+                else -> paceFragment
+            }
+
+            supportFragmentManager.commit {
+                replace(R.id.ctn_fragment, fragment)
+                setReorderingAllowed(true)
+            }
+
+            false
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.statusBarColor = Color.parseColor("#F6EFEC")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
