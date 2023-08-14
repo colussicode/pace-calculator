8def6f5cb2fc91f0f7e3d430be920963dcb7a642
Douglas Gonçalves <cursosinfinitos95@gmail.com>
2023-08-14 23:26:08 +0100

    Tentei fazer o inicio da database, mas nao consegui finalizar e acrescentei o Main_Activity_pace com o calculo.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -41,6 +43,21 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.room:room-common:2.5.2")
+
+    dependencies {
+        val roomVersion = "2.4.1"
+
+        implementation("androidx.room:room-runtime:$roomVersion")
+        kapt("androidx.room:room-compiler:$roomVersion")
+        annotationProcessor("androidx.room:room-compiler:$roomVersion")
+
+        // optional - Test helpers
+        testImplementation("androidx.room:room-testing:$roomVersion")
+
+
+    }
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
